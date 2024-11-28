782797cb19e84d03c68db04596ecdc1b34bd55e9
jalal hemmo <jalal.i.hemmo@gmail.com>
2024-11-28 22:27:13 +0200

    myfirst Java assginment

---
+++ Java fundamentals/Greetings/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("my name is AXSOS Academy");
+        System.out.println("Im 5 years old");
+        System.out.println("My home town is Ramallah , Palestine");
+    }
+}
