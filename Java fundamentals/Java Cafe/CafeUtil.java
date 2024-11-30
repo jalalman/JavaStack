dfd57037b897401172acc51314bb785acf5232c7
jalal hemmo <jalal.i.hemmo@gmail.com>
2024-11-30 11:18:18 +0200

    ninja bonces

--- Java fundamentals/Java Cafe/CafeUtil.java
+++ Java fundamentals/Java Cafe/CafeUtil.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class CafeUtil {
 
-     int getStreakGoal(int numWeeks){
+    int getStreakGoal(int numWeeks){
         int sum=0;
         for(int i =1; i<=numWeeks; i++){
             sum+=i;
@@ -32,5 +32,46 @@ public class CafeUtil {
         System.out.println("Hello "+username);
         System.out.println(String.format("there is %d in front of you ", customers.size()));
     }
+
+    void printPriceChart(String product,double price ,int maxQuantity){
+        if (maxQuantity<=3){
+            System.out.println(product);
+        for(int i =1 ;i <= maxQuantity ; i++){
+            if (i == 1){
+                System.out.printf("%d - $ %.2f \n",i,(price*i));
+            }
+            else{
+                System.out.printf("%d - $ %.2f \n",i,(price*i)-0.5);
+            }
+            }
+        }
+        else{
+        System.out.println("you not allowed to buy more that 3 items per time");
+    }}
+
+    boolean displayMenu(ArrayList<String> menuItems,ArrayList<Double> prices){
+        if (menuItems.size()== prices.size()){
+            for(int i =0 ;i<menuItems.size();i++){
+                System.out.printf("%d %s -- $%.2f \n",i,menuItems.get(i),prices.get(i));
+            }
+            return true;
+        }
+
+        return false;
+    }
+    ArrayList<String> addCustomers(){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> clients=new ArrayList<String>();
+        String username= scanner.nextLine();
+        while(username != "q" || username != "Q"){
+            System.out.println("Please enter your name: ");
+            username= scanner.nextLine();
+            clients.add(username);
+        }
+        return clients;
+    }
 }
+
+
+
 
--- Java fundamentals/Java Cafe/TestCafe.java
+++ Java fundamentals/Java Cafe/TestCafe.java
@@ -9,7 +9,8 @@ public class TestCafe {
       Hint: it will need to correspond with the variable name used below..
     */
     CafeUtil C1= new CafeUtil();
-
+    System.out.println("\n----- Price Chart -----");
+    C1.printPriceChart("coffe",3.5, 3);
         /* ============ App Test Cases ============= */
 
         System.out.println("\n----- Streak Goal Test -----");
@@ -19,15 +20,20 @@ public class TestCafe {
         double[] lineItems = {3.5, 1.5, 4.0, 4.5};
         System.out.printf("Order total: %s \n\n",C1.getOrderTotal(lineItems));
 
-        // System.out.println("----- Display Menu Test-----");
-
+        System.out.println("----- Display Menu Test-----");
+        ArrayList<Double> prices= new ArrayList<Double>();
+        prices.add(3.5);
+        prices.add(2.5);
+        prices.add(1.5);
+        prices.add(5.5);
         ArrayList<String> menu = new ArrayList<String>();
         menu.add("drip coffee");
         menu.add("cappuccino");
         menu.add("latte");
         menu.add("mocha");
         C1.displayMenu(menu);
-
+        System.out.println("-----Overloaded Display Menu Test-----");
+        C1.displayMenu(menu, prices);
         System.out.println("\n----- Add Customer Test-----");
         ArrayList<String> customers = new ArrayList<String>();
         // --- Test 4 times ---
@@ -35,5 +41,6 @@ public class TestCafe {
             C1.addCustomer(customers);
             System.out.println("\n");
         }
+
     }
 }
