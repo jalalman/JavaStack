5448952f96bd08efea545f7abb550e777105e33b
jalalman <jalal.i.hemmo@gmail.com>
2024-12-09 19:37:54 +0200

    pathvar

---
+++ Java Spring/Path Variable/pathvar/src/main/java/com/pathvar/pathvar/PathvarApplication.java
@@ -0,0 +1,13 @@
+package com.pathvar.pathvar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathvarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathvarApplication.class, args);
+	}
+
+}
---
+++ Java Spring/Path Variable/pathvar/src/test/java/com/pathvar/pathvar/PathvarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathvar.pathvar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathvarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
