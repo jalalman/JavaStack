c463875abd8537c7e4c6d3b7aa79883e03d3d7db
jalalman <jalal.i.hemmo@gamil.com>
2024-12-17 00:11:15 +0200

    render all books

---
+++ Java Spring/RenderAll books/books/src/main/java/com/library/books/BookController.java
@@ -0,0 +1,40 @@
+package com.library.books;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.library.books.models.Book;
+import com.library.books.services.BookService;
+
+
+
+@Controller
+public class BookController {
+    private final BookService bookService;
+    
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+    
+    @RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+    public String viewBook(Model model, @PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        model.addAttribute("book", book);
+        
+        return "index.jsp";
+    }
+    
+    @RequestMapping(value="/books", method=RequestMethod.GET)
+    public String viewAllBook(Model model) {
+        List<Book> books = bookService.allBooks();
+        model.addAttribute("books", books);
+        return "all_books.jsp";
+    }
+    
+}
---
+++ Java Spring/RenderAll books/books/src/main/java/com/library/books/BooksApi.java
@@ -0,0 +1,61 @@
+package com.library.books;
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.library.books.models.Book;
+import com.library.books.services.BookService;
+
+
+@RestController
+public class BooksApi {
+    private final BookService bookService;
+    public BooksApi(BookService bookService){
+        this.bookService = bookService;
+    }
+    @RequestMapping("/api/books")
+    public List<Book> index() {
+        return bookService.allBooks();
+    }
+    
+    @RequestMapping(value="/api/books", method=RequestMethod.POST)
+    public Book create(@RequestParam(value="title") String title, @RequestParam(value="description") String desc, @RequestParam(value="language") String lang, @RequestParam(value="pages") Integer numOfPages) {
+        Book book = new Book(title, desc, lang, numOfPages);
+        System.out.println(title);
+        bookService.createBook(book);
+        return book;
+    }
+    
+    @RequestMapping("/api/books/{id}")
+    public Book show(@PathVariable("id") Long id) {
+        Book book = bookService.findBook(id);
+        return book;
+    }
+    @RequestMapping("/api/books/{id}/delete")
+    public String delete(@PathVariable("id") Long id) {
+        bookService.deleteUserById(id);
+         return "done";
+    }
+
+    @RequestMapping(value="/api/books/{id}/{title}", method=RequestMethod.PUT)
+    public String update(@PathVariable ("id") Long id,@PathVariable ("title") String title) {
+        return new String();
+    }
+    
+    @RequestMapping(value="/api/books/{id}", method=RequestMethod.PUT)
+    public Book update(
+            @PathVariable("id") Long id, 
+            @RequestParam(value="title") String title, 
+            @RequestParam(value="description") String desc, 
+            @RequestParam(value="language") String lang,
+            @RequestParam(value="pages") Integer numOfPages) {
+        Book book = bookService.updateBook(id, title, desc, lang, numOfPages);
+        return book;
+    }
+    
+}
